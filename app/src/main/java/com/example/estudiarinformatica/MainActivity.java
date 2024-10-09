010b488a395c97c91dece04c4fc1b87d0157d5cf
Isaac <isaacmartin.dev@gmail.com>
2024-10-09 16:06:47 +0200

    Final commit

--- app/src/main/java/com/example/estudiarinformatica/MainActivity.java
+++ app/src/main/java/com/example/estudiarinformatica/MainActivity.java
@@ -4,11 +4,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaración de String para almacenar la cadena resultado
+    String cadenaResultado;
+
+    // Declaración de EditText para capturar la cadena
+    TextView txtResultado;
 
     // Declaración de Spinner
     Spinner spnCiclos;
@@ -20,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inicialización de EditText
+        txtResultado = findViewById(R.id.txtResultado);
+
         // Inicialización de los objetos Spinner acoplando los recursos XML
         spnCiclos = findViewById(R.id.spnCiclos);
         spnPoblacion = findViewById(R.id.spnPoblacion);
@@ -30,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapterSpinnerPoblacion = ArrayAdapter.createFromResource(this, R.array.poblacion, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapterSpinnerTipo = ArrayAdapter.createFromResource(this, R.array.tipo, android.R.layout.simple_spinner_item);
 
-        // PASO 2 > Especificar el layout que se mostrará cuando se desplieguen las opciones
+        // PASO 2 > Especificar el layout que se mostrará cuando se desplieguen las opciones (SE PUEDE OMITIR)
         adapterSpinnerCiclos.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         adapterSpinnerPoblacion.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         adapterSpinnerTipo.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
@@ -43,13 +55,49 @@ public class MainActivity extends AppCompatActivity {
         spnCiclos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                actualizarTexto();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
+
+        spnPoblacion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                actualizarTexto();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        spnTipo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                actualizarTexto();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        // Listener para el Button BORRAR (DIRECTO)
+        findViewById(R.id.btnBorrar).setOnClickListener(v -> borrarTexto());
+
+    }
+
+    public void borrarTexto(){
+        txtResultado.setText("");
+    }
+
+    public void actualizarTexto(){
+        cadenaResultado = spnCiclos.getSelectedItem().toString()
+                + " en " + spnPoblacion.getSelectedItem().toString()
+                + " de forma " + spnTipo.getSelectedItem().toString();
+        txtResultado.setText(cadenaResultado);
     }
 }
